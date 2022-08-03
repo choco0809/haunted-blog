eadcbf21af0cb073baed1a94bb3ac0bc311d3c7d
choco <superbeaver.180809@gmail.com>
2022-08-04 00:07:45 +0900

    [update] ブログのオーナーとログインユーザーが一致しているかの判断をbefore_actionで実施するよう変更

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -4,6 +4,7 @@ class BlogsController < ApplicationController
   skip_before_action :authenticate_user!, only: %i[index show]
 
   before_action :set_blog, only: %i[show edit update destroy]
+  before_action :check_owned_by, only: %i[edit update destroy]
 
   def index
     @blogs = Blog.search(params[:term]).published.default_order
@@ -17,9 +18,7 @@ class BlogsController < ApplicationController
     @blog = Blog.new
   end
 
-  def edit
-    raise ActiveRecord::RecordNotFound if !@blog.owned_by?(current_user)
-  end
+  def edit; end
 
   def create
     @blog = current_user.blogs.new(blog_params)
@@ -32,7 +31,6 @@ class BlogsController < ApplicationController
   end
 
   def update
-    raise ActiveRecord::RecordNotFound if !@blog.owned_by?(current_user)
     if @blog.update(blog_params)
       redirect_to blog_url(@blog), notice: 'Blog was successfully updated.'
     else
@@ -54,5 +52,9 @@ class BlogsController < ApplicationController
 
   def blog_params
     params.require(:blog).permit(:title, :content, :secret, :random_eyecatch)
+  end
+
+  def check_owned_by
+    raise ActiveRecord::RecordNotFound if !@blog.owned_by?(current_user)
   end
 end
