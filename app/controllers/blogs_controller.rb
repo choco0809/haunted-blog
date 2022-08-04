f2a49e556af64292fbc7d29de5feff3c6352755f
choco <superbeaver.180809@gmail.com>
2022-08-04 19:53:33 +0900

    [update] 通常ユーザーはアイキャッチを設定できないように変更

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -51,7 +51,11 @@ class BlogsController < ApplicationController
   end
 
   def blog_params
-    params.require(:blog).permit(:title, :content, :secret, :random_eyecatch)
+    if current_user.premium?
+      params.require(:blog).permit(:title, :content, :secret, :random_eyecatch)
+    else
+      params.require(:blog).permit(:title, :content, :secret)
+    end
   end
 
   def check_owned_by
